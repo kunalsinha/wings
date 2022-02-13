f819bebdfb334eed57de5f231edb1c76636aa8b9
Kunal Sinha <kunalsinha4u@gmail.com>
2022-02-13 23:47:09 +0530

    improved numerical stability and clipped exponentials to prevent underflow

--- nn/modules/functional.py
+++ nn/modules/functional.py
@@ -3,10 +3,40 @@ import numpy as np
 def sigmoid(X):
     """
     Implements a numerically stable sigmoid function.
+
+    Args:
+        X (array): input
+
+    Returns:
+        array of same shape as X
     """
-    return np.where(X >= 0, 1 / (1 + np.exp(-X)), np.exp(X) / (1 + np.exp(X)))
+    try:
+        # clip to prevent underflow
+        X = np.clip(X, -700, 700)
+        mask = (X >= 0)
+        res = np.empty_like(X)
+        # calculate sigmoid for X >= 0
+        res[mask] =  1 / (1 + np.exp(-X[mask]))
+        # calculate sigmoid for X < 0
+        t = np.exp(X[~mask])
+        res[~mask] = t / (1 + t)
+        return res
+    except Exception:
+        import traceback
+        print(traceback.format_exc())
+        import sys
+        sys.exit(0)
 
 def sigmoid_derivative(X):
-    derivative = sigmoid(X)
-    return derivative * (1 - derivative)
+    """
+    Calculates derivative of sigmoid(X)
+
+    Args:
+        X (array): input
+
+    Returns:
+        array of same shape as X
+    """
+    sig_x = sigmoid(X)
+    return sig_x * (1 - sig_x)
 
