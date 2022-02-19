23bcfb3b9e76fc197c3b4366724cac9e2303f058
Kunal Sinha <kunalsinha4u@gmail.com>
2022-02-19 13:15:55 +0530

    removed dropping of nodes during test time in dropout layer

--- nn/modules/dropout.py
+++ nn/modules/dropout.py
@@ -21,10 +21,14 @@ class Dropout(Module):
         """
         Forward propagates through the dropout layer.
         """
-        N, H = X.shape
-        self.mask = np.random.rand(N, H) > self.prob
-        # print(self.mask)
-        return X * self.mask
+        if self._mode == "train":
+            self.mask = np.random.rand(*X.shape) > self.prob
+            # E[X] = p*0 + (1-p)*X = (1-p)*X
+            # So divide by (1-p) to ensure consistency during test time
+            out = (X * self.mask) / (1 - self.prob)
+        else:
+            out = X
+        return out
 
     def backward(self, dout):
         """
